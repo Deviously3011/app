775467490853f26cdd9c688cc44ac81161a1321b
dejah marshall <2165302@talnet.nl>
2024-03-05 14:49:09 +0100

    up

--- database/seeders/ArticlesTableSeeder.php
+++ database/seeders/ArticlesTableSeeder.php
@@ -3,17 +3,17 @@
 use Illuminate\Database\Seeder;
 use App\Models\Article;
 use Faker\Factory as Faker;
-
 class ArticlesTableSeeder extends Seeder
 {
     public function run()
     {
-        // Clear existing records to start from scratch
+        // Let's truncate our existing records to start from scratch.
         Article::truncate();
 
-        // Create dummy data using Faker
         $faker = Faker::create();
-        foreach (range(1, 50) as $index) {
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
             Article::create([
                 'title' => $faker->sentence,
                 'body' => $faker->paragraph,
